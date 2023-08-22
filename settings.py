05e2a378769f39d9ab2f1a159ffdad67fd328e96
StefanOswald03 <55189398+StefanOswald03@users.noreply.github.com>
2023-08-22 22:10:26 +0200

    start implementing logging

--- settings.py
+++ settings.py
@@ -14,3 +14,46 @@ COGS_DIR = BASE_DIR / "cogs"
 
 SPOTIFY_CLIENT = os.getenv("SPOTIFY_CLIENT")
 SPOTIFY_PASSWORD = os.getenv("SPOTIFY_PASSWORD")
+
+LOGGING_CONFIG = {
+    "version": 1,
+    "disabled_existing_Loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "%(levelname)-10s - %(asctime)s - %(module)-15s : %(message)s"
+        },
+        "standard": {
+            "format": "%(levelname)-105 - %(name)-15s : %(message)s"
+        }
+    },
+    "handlers": {
+        "console": {
+            'Level': 'DEBUG',
+            'class': "logging. StreamHandler",
+            "formatter": "simple"
+        },
+        'console2': {
+            'Level': "WARNING",
+            'class': "Logging.StreamHandler",
+            'formatter': "simple"
+        },
+        "file": {
+            'Level': "INFO",
+            'class': "logging.FileHandler",
+            "filename": "Logs/infos. Log",
+            'mode': "w"
+        }
+    },
+    "Loggers": {
+        "bot": {
+            'handlers': ['console'],
+            "Level": "INFO",
+            "propagate": False
+        },
+        "discord": {
+            "handlers": ["console2", "file"],
+            "Level": "INFO",
+            "propagate": False
+        }
+    }
+}
